140d71944587d92c19fd8655c5c6f34f12458dc2
jellyboston <bostonjake2004@gmail.com>
2025-09-06 17:22:10 -0400

    saving i wanna eat

--- PSET1/SCCs/scc.py
+++ PSET1/SCCs/scc.py
@@ -22,17 +22,41 @@ def reset_color(og_ordering):
     for result in og_ordering:
         result.color = 0
 
-def extract_reverse_finished(og_ordering):
-    assert g.size == len(og_ordering), "use interchangeably"
-    reverse = [[] for i in range(g.size)]
-    for i in range(g.size):
-        reverse[i] = og_ordering[i].f
-    reverse.sort(reverse=True)
+'''
+Returns:
+    list: vertex ids sorted by descending finished time
+'''
+def extract_reverse_order(og_ordering, g):
+    assert g.size() == len(og_ordering), "use interchangeably"
+    reverse = []
+    for v in range(g.size):
+        assert og_ordering[v].finish != None
+        reverse[v] = (og_ordering[v].finish, v)
+    reverse.sort(reverse=True) # sorts descending by first element (finish)
+    new_order = [v for f, v in reverse]
+    return new_order
+
+'''
+Return:
+    list: vertices within a SCC
+'''
+def second_pass_dfs(T_graph, start_vertex, seen):
+    edges = T_graph.edges(start_vertex)
+    seen[start_vertex] = True
+    scc = [start_vertex]
+    for v in edges:
+        if not seen[v]:
+            # TODO: fix the logic here
+            scc.append(second_pass_dfs(T_graph, v, seen))
+    return scc
 
 
 def sccs(g):
     # YOUR SOLUTION HERE
-    # Obtain the finish order of the og graph
+    '''
+    Obtain the finish order of the og graph
+    NOTE: index in results = vertex id
+    '''
     og_ordering = g.dfs()
 
     # Reverse the edges to obtain the transposed (T_graph)
@@ -40,13 +64,10 @@ def sccs(g):
     reset_color(og_ordering) # might not be needed
 
     # Run DFS again in reverse finished order
-    reverse = extract_reverse_finished(og_ordering) # FIX
-    '''
-    TODO: fix this reverse! (see cg)
-    '''
-
-
-
-
-
-    return []
+    reverse_order = extract_reverse_order(og_ordering, g) 
+    seen = [False] * g.size()
+    sccs = []
+    for v in reverse_order:
+        if not seen[v]:
+            sccs.append(second_pass_dfs(T_graph, v, seen))
+    return sccs
