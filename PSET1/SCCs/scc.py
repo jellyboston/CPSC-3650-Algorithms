4a97c9a2a34dab018841fe67353b24e0febbd986
jellyboston <bostonjake2004@gmail.com>
2025-09-07 21:26:15 -0400

    feat: kosaraju's algo. functional draft: - implemented second pass DFS traversal

--- PSET1/SCCs/scc.py
+++ PSET1/SCCs/scc.py
@@ -11,8 +11,8 @@ Kosaraju's Algorithm:
 '''
 def transpose_graph(g):
     # New transpose graph that stores rev. edges
-    reversed_graph = Digraph(g.size)
-    for v in range(g.size):
+    reversed_graph = Digraph(g.size())
+    for v in range(g.size()):
         v_edges = g.edges(v)
         for e in v_edges: # 1D array
             reversed_graph.add_edge(e, v)
@@ -29,7 +29,7 @@ Returns:
 def extract_reverse_order(og_ordering, g):
     assert g.size() == len(og_ordering), "use interchangeably"
     reverse = []
-    for v in range(g.size):
+    for v in range(g.size()):
         assert og_ordering[v].finish != None
         reverse[v] = (og_ordering[v].finish, v)
     reverse.sort(reverse=True) # sorts descending by first element (finish)
@@ -47,27 +47,24 @@ def second_pass_dfs(T_graph, start_vertex, seen):
     for v in edges:
         if not seen[v]:
             # TODO: fix the logic here
-            scc.append(second_pass_dfs(T_graph, v, seen))
+            sub = second_pass_dfs(T_graph, v, seen)
+            scc.extend(sub)
     return scc
 
-
 def sccs(g):
-    # YOUR SOLUTION HERE
-    '''
-    Obtain the finish order of the og graph
-    NOTE: index in results = vertex id
-    '''
+    # Obtain the finish order of the og graph (index is vertex id)
     og_ordering = g.dfs()
 
     # Reverse the edges to obtain the transposed (T_graph)
     T_graph = transpose_graph(g)
-    reset_color(og_ordering) # might not be needed
+    # reset_color(og_ordering)
 
     # Run DFS again in reverse finished order
     reverse_order = extract_reverse_order(og_ordering, g) 
     seen = [False] * g.size()
-    sccs = []
+    components = []
     for v in reverse_order:
         if not seen[v]:
-            sccs.append(second_pass_dfs(T_graph, v, seen))
+            scc = second_pass_dfs(T_graph, v, seen)
+            components.append(scc)
     return sccs
