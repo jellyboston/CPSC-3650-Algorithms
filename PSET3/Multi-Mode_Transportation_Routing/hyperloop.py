4445ce8bf9ae55263323da132ebce75b10f4c65a
jellyboston <bostonjake2004@gmail.com>
2025-10-04 23:52:25 -0400

    feat: compelted implementation draft before testing

--- PSET3/Multi-Mode_Transportation_Routing/hyperloop.py
+++ PSET3/Multi-Mode_Transportation_Routing/hyperloop.py
@@ -1,4 +1,4 @@
-from graph import Graph
+from digraph import Digraph
 '''
     High level callouts:
     - we establish two different layers: layer 0 (coupon unused) and 1 (used) within the graph
@@ -19,24 +19,25 @@ It is the hyperloop weights in the second loop that connect
 these two layers together. 
 '''
 def fill_expanded_graph(H, V, car, loop):
+    OFFSET = V
     # translate into diretcted graph by adding forward/back edges
     for u in range(V):
         for v, w in car.edges(u):
             # process each undirected edge once
             if u < v: 
-                # add layer of car edges
+                # add car edges to each layer
                 H.add_edge(L0(u), L0(v), w)
                 H.add_edge(L0(v), L0(u), w)
-
-                # layer of only hyperloop edges
-                H.add_edge(L1(u), L1(v), w)
-                H.add_edge(L1(v), L1(u), w)
+                H.add_edge(L1(u, OFFSET), L1(v, OFFSET), w)
+                H.add_edge(L1(v, OFFSET), L1(u, OFFSET), w)
     
     # fill with possible car <-> hyperloop connected vertices with hyperloop weights
     for u in range(V):
         for v, w in loop.edges(u):
-            H.add_edge(L0(u), L1(v), w)
-            H.add_edge(L1(v), L0(u), w)
+            # again, avoid double adding
+            if u < v:
+                H.add_edge(L0(u), L1(v, OFFSET), w)
+                H.add_edge(L0(v), L1(u, OFFSET), w)
 
 '''
 dijsktra explores all possibilities. we must consider two edge cases:
@@ -44,11 +45,19 @@ dijsktra explores all possibilities. we must consider two edge cases:
     2. the hyperloop was used (dest. will be in layer 1)
 Take the fastest of the two as they were both existing possibilities found by Dijsktra's.
 '''    
-def expanded_dijsktra(H, source, dest):
+def expanded_dijsktra(H, V, source, dest):
+    OFFSET = V
     sssp = H.dijkstra(L0(source))
-    fastest_no_coupon = sssp[L0(dest)].d
-    fastest_used_coupon = sssp[L1(dest)].d
-    return min(fastest_no_coupon, fastest_used_coupon)
+
+    # dest node in each of the layers
+    end0, end1 = L0(dest), L1(dest, OFFSET)
+    fastest_no_coupon = sssp[end0].d
+    fastest_used_coupon = sssp[L1(end1)].d
+
+    # edge case: no routes
+    if fastest_no_coupon == float('inf') and fastest_used_coupon == float('inf'):
+        return None, None, None
+    return sssp, min(fastest_no_coupon, fastest_used_coupon)
 
 def find_route(car, loop, source, dest):
     """ Finds the shortest path from sourse to destination and returns it as a list
@@ -78,16 +87,32 @@ def find_route(car, loop, source, dest):
 
     # build expanded graph H to hold car/loop vertices (size 2V)
     V = car.size()
-    expanded_graph = Graph(2 * V)
+    expanded_graph = Digraph(2 * V)
 
     # add car edges to both layers; connect layers with the hyperloop edges
     fill_expanded_graph(expanded_graph, V, car, loop)
 
-    # run dijsktra starting from source layer 0
-    shortest_to_dest = expanded_dijsktra(expanded_graph, source, dest)
-
+    # run dijsktra starting from (source, layer 0)
+    sssp_tree, end = expanded_dijsktra(expanded_graph, V, source, dest)
+    if sssp_tree is None:
+        return []
+    
     # trace back to identify the parent of each node and format to exp output
-    
-    
+    results = []
+    node = end
+    while sssp_tree.parent[node] is not None or node != source:
+        parent = sssp_tree.parent[node]
+        if parent is None:
+            break
+        # if expanded, convert offset indices back to og vertex ids
+        u = parent % V
+        v = node % V
 
-    return [] # here to keep Python validator happy (routing.py imports hyperloop.py even though not used for checking C++ output)
+        # format
+        mode = "hyperloop" if (parent < V and node >= V) else "car"
+        results.append((u, v, mode))
+
+        # increment next
+        node = parent
+    results.reverse()
+    return results
