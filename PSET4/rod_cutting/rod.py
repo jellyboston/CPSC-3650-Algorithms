15e1ed3fa7573ed8abb62d17ee688bd6869c2c1f
jellyboston <bostonjake2004@gmail.com>
2025-10-20 22:49:39 -0400

    feat: final implement and memoization practice - passed test cases on Ed - experimented with LRU cache memoizer and compared timing with dp

--- PSET4/rod_cutting/rod.py
+++ PSET4/rod_cutting/rod.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 def cut_rod(n, price, m):
     """ Returns the maximum value after cutting a rod of length n, given
         the price of rod segments and the cost of a cut.  Assumes cuts can't
@@ -20,7 +22,41 @@ def cut_rod(n, price, m):
         # values[i] = float('-inf')
         values[i] = price[i]
         for c in range(1, i-1): # must account for 1 at the edges (constraint)
-            remaining_cut = i - c - 1 # 1 unit wasted per cut (constraint)
-            if price[c] + values[remaining_cut] > values[i]:
-                values[i] = price[c] + values[remaining_cut]
+            # 1 unit wasted per cut (constraint)
+            remaining_cut = i - c - 1 
+            if remaining_cut < 0: continue
+            # subtract cost (constraint)
+            subproblem = price[c] + values[remaining_cut] - m
+            if subproblem > values[i]: 
+                values[i] = subproblem
     return values[n]
+
+'''
+Experimental implementation using memoization. A friend
+showed me how to use the lru_cache decorator and now im obsessed.
+
+Timing analysis:
+DP approach:
+0.14590555988252163
+LRU cache:
+0.22687724977731705
+
+Results:
+- in python, DP approach is useually more efficient (LRU has to setup cache decorator wrapping,
+recursive stack creations and function calls)
+- but in Java/C apparently they should both tie 
+'''
+def cut_rod_experiment_memoization(n, price, m):
+    @lru_cache(maxsize=None)
+    def best_value(i):
+        if i <= 0:
+            return 0
+        value_i = price[i]
+        for c in range(1, i - 1):
+            remaining_cut = i - c - 1
+            if remaining_cut < 0: continue
+            sub = price[c] + best_value(remaining_cut) - m
+            if sub > value_i:
+                value_i = sub
+        return value_i
+    return best_value(n)
