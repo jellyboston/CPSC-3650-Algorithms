ae8e26e9559c380cda64713127cd83980d37a2bb
jellyboston <bostonjake2004@gmail.com>
2025-10-31 10:29:58 -0400

    plan: drew out example and solidified pseudocode

--- PSET5/find_min_in_tree/tree_min.py
+++ PSET5/find_min_in_tree/tree_min.py
@@ -4,4 +4,9 @@ def find_min_in_tree(g):
         g -- a complete binary tree as an undirected graph
     """
     # YOUR SOLUTION HERE
+    '''
+    Pseudocode
+    divide: conceptually separate graph into one smaller subtree
+    conquer: recurse into subtree until local min is found and return
+    '''
     return (0, 56.961247)
