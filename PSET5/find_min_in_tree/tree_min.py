baa5008e0496755fbf09c6b0c179c9fedf531081
jellyboston <bostonjake2004@gmail.com>
2025-10-31 13:10:17 -0400

    feat: implement div and conquer algorithm for min in binary tree

--- PSET5/find_min_in_tree/tree_min.py
+++ PSET5/find_min_in_tree/tree_min.py
@@ -9,4 +9,34 @@ def find_min_in_tree(g):
     divide: conceptually separate graph into one smaller subtree
     conquer: recurse into subtree until local min is found and return
     '''
-    return (0, 56.961247)
+    def recursive_min(vertex, parent):
+        # get all neighbors
+        nbrs = [u for u in g.edges(vertex) if u != parent]
+
+        # base case: found the local minimum
+        local_min = True
+        for c in nbrs:
+            if g.value(vertex) > g.value(c):
+                local_min = False
+                break
+        if local_min:
+            return vertex
+        
+        # recurse onto the smallest neighbor
+        candidate = vertex
+        for c in nbrs:
+            if g.value(c) < g.value(candidate):
+                candidate = c
+        if nbrs and g.value(candidate) < g.value(vertex):
+                return recursive_min(candidate, vertex)
+        
+        # handle plateau
+        return vertex
+        
+    if g.size() == 1:
+        return 0, g.value(0)
+    
+    # pick any start vertex
+    start = 0
+    min_index = recursive_min(start, None)
+    return min_index, g.value(min_index)
