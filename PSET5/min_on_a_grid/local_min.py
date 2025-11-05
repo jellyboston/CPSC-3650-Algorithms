0428abf96c94d378521b8e982cc49b8667469c3d
jellyboston <bostonjake2004@gmail.com>
2025-11-04 21:06:14 -0500

    fix: add offset logic and OOB/edge case guards - change split_min() parameters to incl. r0 and c0 offsets - offset logic allows us to keep track of where a point in the subgraph lies in the entire graph (for use in neighbor checking) - add OOB guards in is_local_min() - add edge case col == 0 guard in split_min()

--- PSET5/min_on_a_grid/local_min.py
+++ PSET5/min_on_a_grid/local_min.py
@@ -14,75 +14,63 @@ def find_min(grid):
 
         # test each pivot direction for other local mins
         for p_r, p_c in pivots:
-            test_val = grid[r + p_r][c + p_c]
-            if test_val < candidate_val: # we found a smaller neighbor
-                return False
+            r_dest, c_dest = r + p_r, c + p_c
+            # guard OOB neighbors
+            if 0 <= r_dest < rows and 0 <= c_dest < cols: 
+                test_val = grid[r_dest][c_dest]
+                if test_val < candidate_val: # we found a smaller neighbor
+                    return False
         return True
     
-    def split_min(split_grid):
+    '''
+    recursing on subgrid effectively resets pointer to (0,0), but we need
+    to keep track of original grid indices for testing neighbors w.r.t full grid: 
+
+    r0 - row offset that if added would map to original grid row
+    c0 - col fofset that if added would map to original grid col
+    '''
+    def split_min(split_grid, r0, c0):
         # base case: small Kx2 subgrid (height doesn't shrink so idc)
         rows, cols = len(split_grid), len(split_grid[0])
+        if cols == 0:
+            return None
         if cols <= 2:
             # search each cell for local min
             for r in range(rows):
                 for c in range(cols):
-                    if is_local_min(split_grid, r, c):
-                        return split_grid[r][c]
+                    if is_local_min(grid, r0 + r, c0 + c) == True:
+                        return grid[r0 + r][c0 + c]
                     
         # recursive case: repeat split logic down the middle
         split = cols // 2
         min_row, min_val = 0, float('inf')
         for r in range(rows):
-            val = grid[r][split]
+            val = split_grid[r][split]
             if val < min_val:
                 min_row, min_val = r, val
         # decide whether to conquer l or r subgraph (prevent OOB)
-        left_val = grid[min_row][split - 1] if split - 1 >= 0 else float('inf')
-        right_val = grid[min_row][split + 1] if split + 1 < cols else float ('inf')
+        left_val = split_grid[min_row][split - 1] if split - 1 >= 0 else float('inf')
+        right_val = split_grid[min_row][split + 1] if split + 1 < cols else float ('inf')
 
         # base case: check if min_val candidate itself is a local min
-        if is_local_min(grid, min_row, split) == True:
+        if is_local_min(grid, r0 + min_row, c0 + split) == True:
             return min_val
         # recurse on the left
         elif left_val <= right_val:
-            split_grid = grid[0:rows-1][0:split-1]
-            return split_min(split_grid)
+            # list comprehension -> row[:split] represents the cols we pick
+            split_grid_left = [row[:split] for row in split_grid]
+            return split_min(split_grid_left, r0, c0)
         # recurse on the right
         else:
-            split_grid = grid[0:rows-1][split+1:cols-1]
-            return split_min(split_grid)
+            split_grid_right = [row[split+1:] for row in split_grid]
+            return split_min(split_grid_right, r0, c0 + split + 1)
 
     '''
     Divide and conquer:
     - Divide: Split grid down the middle and search for min
     - Conquer: Recurse across smaller neighbor until base case is hit
-        - Base Case: 2x2 grid where we can check directly for local min
+        - Base Case: Kx2 grid where we can check directly for local min
     - Combine: return value (no merge)
-    
     '''
     # YOUR SOLUTION HERE
-    rows, cols = len(grid), len(grid[0])
-    split = cols // 2
-
-    # find candidate min in middle col
-    min_row, min_val = 0, float('inf')
-    for r in range(rows):
-        val = grid[r][split]
-        if val < min_val:
-            min_row, min_val = r, val
-
-    # decide whether to conquer l or r subgraph (prevent OOB)
-    left_val = grid[min_row][split - 1] if split - 1 >= 0 else float('inf')
-    right_val = grid[min_row][split + 1] if split + 1 < cols else float ('inf')
-
-    # base case: check if min_val candidate itself is a local min
-    if is_local_min(grid, min_row, split) == True:
-        return min_val
-    # recurse on the left
-    elif left_val <= right_val:
-        split_grid = grid[0:rows-1][0:split-1]
-        return split_min(split_grid)
-    # recurse on the right
-    else:
-        split_grid = grid[0:rows-1][split+1:cols-1]
-        return split_min(split_grid)
+    return split_min(grid, 0, 0)
