3e3ff56653b6424dace242fcec2e2e9667b85eb4
jellyboston <bostonjake2004@gmail.com>
2025-12-05 21:11:42 -0500

    feat: adjacency check and count vertex

--- PSET7/Verifier/verification.py
+++ PSET7/Verifier/verification.py
@@ -6,4 +6,33 @@ def verify(g, path):
         path -- a path in g represented as a list of vertices
     """
     # YOUR SOLUTION HERE
+    # enforce valid path (w/ adjacency check)
+    for idx in range(len(path) - 1):
+        u = path[idx]
+        v = path[idx+1]
+        if not g.has_edge(u, v):
+            return False # path has invalid edge
+    
+    '''
+    APPROX-HP invariant:
+    1. visits |v| - 1 vertices once
+    2. visits remaining vertex **at least once** (>= ok)
+    3. does not traverse any edge more than once
+    '''
+    count_vertex = [0] * g.size()
+    for v in path:
+        count_vertex[v] += 1
+    # verify
+    remaining_vertex = -1
+    found = False
+    for v in count_vertex:
+        # if some other vertex is visited multiple times
+        if v != remaining_vertex and found and count_vertex[v] >= 2:
+            return False
+        elif count_vertex[v] >= 2:
+            found = True
+            remaining_vertex = v
+        else:
+            continue
+    
     return False
